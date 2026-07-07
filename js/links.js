de79770fd561e916312cfbdcb7265a9f0d4815af
RookieMaster69 <terrgff8@gmail.com>
2026-07-07 14:31:37 +0800

    admin: update links

--- js/links.js
+++ js/links.js
@@ -1,43 +1,43 @@
 /* ADMIN-MANAGED. 由後台 admin.html 管理。手動編輯請維持 JSON 格式：鍵要雙引號，資料結尾必須是 ]; 後面不要加任何東西。 */
 const LINKS = [
   {
-    "label": "GITHUB",
-    "sublabel": "code & projects",
-    "url": "https://github.com/terrgff8",
+    "label": "寵物領養問卷",
+    "sublabel": "領養貓貓請先填這張問卷唷",
+    "url": "https://forms.gle/Lx4o5F7QXfkL8edW9",
+    "icon": "link",
+    "highlight": true
+  },
+  {
+    "label": "Line個人助理",
+    "sublabel": "開源分享",
+    "url": "https://github.com/terrgff8/Line-assis-bot",
     "icon": "github",
     "highlight": false
   },
   {
-    "label": "PORTFOLIO",
-    "sublabel": "selected works",
-    "url": "https://example.com/portfolio",
-    "icon": "globe",
+    "label": "要吃啥!?大轉盤",
+    "sublabel": "解決吃飯選擇困難",
+    "url": "https://github.com/terrgff8/Line-assis-bot",
+    "icon": "link",
     "highlight": false
   },
   {
-    "label": "X / TWITTER",
-    "sublabel": "daily thoughts",
-    "url": "https://x.com/placeholder",
-    "icon": "x",
+    "label": "報價單產生器",
+    "sublabel": "一鍵生產報價單",
+    "url": "https://terrgff8.github.io/price-generator",
+    "icon": "file",
     "highlight": false
   },
   {
-    "label": "INSTAGRAM",
-    "sublabel": "visual log",
-    "url": "https://instagram.com/placeholder",
+    "label": "Instergram",
+    "sublabel": "我的IG",
+    "url": "https://www.instagram.com/tainancat.waytohome/",
     "icon": "instagram",
     "highlight": false
   },
   {
-    "label": "YOUTUBE",
-    "sublabel": "video channel",
-    "url": "https://youtube.com/@placeholder",
-    "icon": "youtube",
-    "highlight": false
-  },
-  {
     "label": "EMAIL",
-    "sublabel": "direct contact",
+    "sublabel": "合作請聯繫",
     "url": "mailto:terrgff8@gmail.com",
     "icon": "mail",
     "highlight": false
