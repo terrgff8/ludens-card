cea776aa3b14c6d4d06974c2414ce5fb5a02fd90
RookieMaster69 <terrgff8@gmail.com>
2026-07-08 09:39:24 +0800

    admin: update profile

--- js/profile.js
+++ js/profile.js
@@ -2,6 +2,6 @@
 const PROFILE = {
   "name": "吉貓",
   "tagline": "Porter / Builder / Creator",
-  "bio": "台南在地貓咪中途",
+  "bio": "台南在地貓咪中途\n兼差救浪浪\n-網頁架站服務\n-AI GEO服務\n-智能機器人串接LINE/Discord/Tg\n以下是我的作品 只要有需求都可以聊聊\n報價合理 服務至上",
   "initial": ""
 };
